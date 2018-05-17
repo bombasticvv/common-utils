0aaeb9e694661f483c5f8eee36647fd3fd69fcdb
admin <1729921197@qq.com>
2018-05-17 10:56:36 +0800

    优化

--- src/main/java/com/utils/part1/test/TestJDBCConnection.java
+++ src/main/java/com/utils/part1/test/TestJDBCConnection.java
@@ -1,13 +1,29 @@
 package com.utils.part1.test;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import com.utils.part1.jdbc.ConnectionPool;
 import com.utils.part1.jdbc.DBbean;
 import com.utils.part1.jdbc.PropertiesUtil;
 
 public class TestJDBCConnection {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException, InterruptedException {
 		DBbean dbBean=getDBbean();
 		ConnectionPool pool=new ConnectionPool(dbBean);
+		Connection conn=pool.getConnection();
+		String sql="select cus_nm,usr_no from t_urm_pinf where reg_mbl='17348522785'";
+		PreparedStatement stmt = conn.prepareStatement(sql);
+		ResultSet rs=stmt.executeQuery();
+		while(rs.next()){
+			System.out.println(rs.getString("cus_nm"));
+			System.out.println(rs.getString("usr_no"));
+		}
+		Thread.sleep(10000);
+		pool.releaseConn(conn);
+		System.out.println("releaseConn");
 	}
 	
 	private static DBbean getDBbean() {
