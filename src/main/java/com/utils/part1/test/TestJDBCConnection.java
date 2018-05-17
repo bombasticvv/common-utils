484012dbb14bf884682a4a1074141d5d78bd6a34
admin <1729921197@qq.com>
2018-05-17 19:39:57 +0800

    添加log4j

--- src/main/java/com/utils/part1/test/TestJDBCConnection.java
+++ src/main/java/com/utils/part1/test/TestJDBCConnection.java
@@ -5,25 +5,41 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+import org.apache.log4j.xml.DOMConfigurator;
+
 import com.utils.part1.jdbc.ConnectionPool;
 import com.utils.part1.jdbc.DBbean;
 import com.utils.part1.jdbc.PropertiesUtil;
 
 public class TestJDBCConnection {
+	
+	private static Logger logger;
+	
+	static{
+		//PropertyConfigurator.configure(TestJDBCConnection.class.getClassLoader().getResource("").getPath()+"conf/log4j.properties");
+		DOMConfigurator.configure(TestJDBCConnection.class.getClassLoader().getResource("").getPath()+"conf/log4j.xml");
+		logger=Logger.getLogger(TestJDBCConnection.class);
+	}
+	
 	public static void main(String[] args) throws SQLException, InterruptedException {
 		DBbean dbBean=getDBbean();
 		ConnectionPool pool=new ConnectionPool(dbBean);
 		Connection conn=pool.getConnection();
-		String sql="select cus_nm,usr_no from t_urm_pinf where reg_mbl='17348522785'";
+		String sql="select cus_nm,usr_no from t_urm_pinf where reg_mbl='13666688888'";
 		PreparedStatement stmt = conn.prepareStatement(sql);
 		ResultSet rs=stmt.executeQuery();
 		while(rs.next()){
 			System.out.println(rs.getString("cus_nm"));
 			System.out.println(rs.getString("usr_no"));
+			logger.info(rs.getString("cus_nm"));
+			logger.info(rs.getString("usr_no"));
 		}
 		Thread.sleep(10000);
 		pool.releaseConn(conn);
 		System.out.println("releaseConn");
+		logger.info("releaseConn");
 	}
 	
 	private static DBbean getDBbean() {
